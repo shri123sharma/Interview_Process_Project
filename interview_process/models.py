8535ebab7dd3055adbe9fbbc857f1ff00f1f90d7
shri123sharma <sshrikant919@gmail.com>
2023-05-22 18:08:11 +0530

    delete question_type models

--- interview_process/models.py
+++ interview_process/models.py
@@ -83,9 +83,10 @@ class Interview(BaseModel):
     def __str__(self):
         return str(self.final_score)
     
-class QuestionType(BaseModel):
-    question=models.CharField(max_length=200,null=True,blank=True)
-    question_type=models.CharField(max_length=100,null=True,blank=True)
+# class QuestionType(BaseModel):
+#     question=models.CharField(max_length=200,null=True,blank=True)
+#     question_type=models.CharField(max_length=100,null=True,blank=True)
 
-    def __str__(self):
-        return self.question_type
+#     def __str__(self):
+#         return self.question_type
+    
